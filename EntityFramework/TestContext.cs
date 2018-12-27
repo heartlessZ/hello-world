75e9fd183bf9558107f0fbcd2ffaf60b306c1afb
赵康辰 <kc.zhao@aisinovision.com>
2018-12-27 12:06:37 +0800

    hello

---
+++ EntityFramework/TestContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework
+{
+    public class TestContext:DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
+        {
+
+        }
+        
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
